87f4a9f3c97ae708c9c2d9931dc7f5949202a0fe
rspringer47 <rspringer47@gmail.com>
2017-07-12 09:59:18 -0600

    completed score; not score multipliers

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,31 @@
 class Scrabble
 
   def score(word)
-    1
+    word_score = 0
+    if word == nil
+      word_score = 0
+    else
+      word.chars.each do |letter|
+        word_score += point_values[letter.upcase]
+      end
+    end
+    word_score
+  end
+
+  def score_with_multipliers(word, multiplier_array,)
+
   end
 
   def point_values
     {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
     }
   end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,29 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
-  def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+
+  def setup
+    @game = Scrabble.new
   end
+
+
+  def test_it_can_score_a_single_letter
+    assert_equal 1, @game.score("a")
+    assert_equal 4, @game.score("f")
+  end
+
+  def test_it_can_score_two_letters
+    assert_equal 5, @game.score("hi")
+    assert_equal 2, @game.score("no")
+    assert_equal 5, @game.score("uh")
+  end
+
+  def test_it_can_score_three_letters
+    assert_equal 6, @game.score("nvr")
+    assert_equal 4, @game.score("gna")
+    assert_equal 7, @game.score("giv")
+    assert_equal 6, @game.score("you")
+    assert_equal 7, @game.score("upp")
+  end
+
 end
